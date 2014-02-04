ad6439e1eebbe455b96d9b431528eea5f1b1f1a0
Daizhe <qidaizhe11@gmail.com>
2014-02-04 14:19:29 +0800

    Tut 05: Objects in Depth, depth buffer.
    Since it's very simple, I didn't create new cpp file.

--- Tut-05-Objects-in-Depth/BaseVertexOverlap.cpp
+++ Tut-05-Objects-in-Depth/BaseVertexOverlap.cpp
@@ -221,12 +221,18 @@ void init()
   glEnable(GL_CULL_FACE);
   glCullFace(GL_BACK);
   glFrontFace(GL_CW);
+
+  glEnable(GL_DEPTH_TEST);
+  glDepthMask(GL_TRUE);
+  glDepthFunc(GL_LEQUAL);
+  glDepthRange(0.0f, 1.0f);
 }
 
 void display()
 {
   glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-  glClear(GL_COLOR_BUFFER_BIT);
+  glClearDepth(1.0f);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
   glUseProgram(the_program);
 
